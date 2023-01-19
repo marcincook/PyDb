02dc053c3024742ef77cfefc2ea5acc36d240e4a
Marcin Dabrowski <marcincook@gmail.com>
2023-01-19 04:53:11 +0100

    Init

--- README.md
+++ README.md
@@ -10,3 +10,13 @@ https://pymssql.readthedocs.io/en/stable/pymssql_examples.html#basic-features-st
 ## Wydanie wersji 
 First create a source distribution with:
 $ python setup.py sdist
+
+
+## Powiadomienia dz∑iękowe 
+https://www.youtube.com/watch?v=5EHEG4gMCNs
+https://versa-syahptr.github.io/winotify/docs/winotify.html
+
+## Window
+https://www.youtube.com/watch?v=iM3kjbbKHQU
+https://github.com/TomSchimansky/CustomTkinter/blob/master/examples/complex_example.py
+
--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import os
 from os import getenv
+from urllib.parse import urlparse, urlencode
+
+import customtkinter
 import pymssql
+from winotify import Notification, audio
+
 from print import print_label
+from win10toast import ToastNotifier
+
+from window import App
+
+customtkinter.set_appearance_mode("Dark")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
 # Zmienne środowiskowe z pliku env
 server = getenv("PYMSSQL_TEST_SERVER")
@@ -54,8 +66,60 @@ def mojaFunkcjaWitaj(name):
     print(f'Witaj, {name}')
 
 
+def pokazToast():
+    t = ToastNotifier()
+    t.show_toast("Tytuł tosta", "Zrobione złotko co sądzisz o wyniku w konsoli", duration=4)
+
+
+def pokazToast2():
+    script_dir = os.path.dirname(__file__)
+    rel_path = "favicon.png"
+    icon = os.path.join(script_dir, rel_path)
+    message = "Wiadomość do wyświetlenia  \nWiadomość do wyświetlenia  \nWiadomość do wyświetlenia  \nWiadomość do wyświetlenia "
+    toast = Notification(app_id="A-DONG Darek",
+                         title="W konsoli pokazuje się status pobrania danych z bazy gastro",
+                         msg=message,
+                         duration="long",
+                         icon=icon,
+                         )
+    # dzwięk
+    toast.set_audio(audio.Mail, loop=False)
+    # Akcja
+    # https://a-donggdansk.pl/admin/orders?selected_order_id=230&archive=1
+    url = "https://a-donggdansk.pl/admin/orders"
+    params = {'selected_order_id': '230', 'archive': '1'}
+
+    # url += ('&' if urlparse(url).query else '?') + urlencode(params)
+    print(url)
+    toast.add_actions(label="Zobacz nowe zamówienie 230", launch=url)
+    toast.show()
+
+
+
+def okienko():
+    customtkinter.set_appearance_mode('dark')
+    customtkinter.set_default_color_theme('dark-blue')
+    root = customtkinter.CTk()
+    root.geometry("500x300")
+    root.title("Tytuł okienka")
+    frame = customtkinter.CTkFrame(master=root)
+    frame.pack(pady=20, padx=20, fill="both", expand=True)
+    label = customtkinter.CTkLabel(master=frame, text="Hello World")
+    label.pack(pady=12, padx=10)
+    # button = customtkinter.CTkButton(master=frame, text="Pokaż tost windows", command=pokazToast)
+    # button.pack(pady=12, padx=10)
+    button2 = customtkinter.CTkButton(master=frame, text="Pokaż tost 2 windows", command=pokazToast2)
+    button2.pack(pady=12, padx=10)
+    root.mainloop()
+
+
 if __name__ == '__main__':
+
     mojaFunkcjaWitaj('Darek')
     puknijDoBazy()
     result_print = print_label('Kaczka dziwaczka', '1 z (3)', 'ID_2344', 'L_332')
     print(result_print)
+    # pokazToast()
+    okienko()
+    # app = App()
+    # app.mainloop()
